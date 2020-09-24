881605653ca0ca55ffe089cff0e298aff63e2f2f
willamy388720 <willamygt@gmail.com>
2020-09-24 00:32:46 -0300

    editing deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,5 +23,5 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w[~/.ssh/id_rsa] }
 server '18.223.209.47', user: 'deploy', roles: %w[web app db]
+set :ssh_options, { forward_agent: true }
