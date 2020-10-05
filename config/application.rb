a2dd386eeaef898e3c3ceff7a4eb7ea777197d6f
willamy388720 <willamygt@gmail.com>
2020-10-05 05:26:22 -0300

    ajusting layout landing page

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,18 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -31,5 +31,12 @@ module MvpRaxae
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # config.assets.paths << Rails.root.join('node_modules', '@fortawesome', 'fontawesome-free', 'webfonts')
+    # config.assets.paths << Rails.root.join('node_modules', '@fortawesome', 'fontawesome-free', 'scss', 'fontawesome')
+    # config.assets.paths << Rails.root.join('node_modules', '@fortawesome', 'fontawesome-free', 'scss', 'solid')
+    # config.assets.paths << Rails.root.join('node_modules', '@fortawesome', 'fontawesome-free', 'scss', 'regular')
+    # config.assets.paths << Rails.root.join('node_modules', '@fortawesome', 'fontawesome-free', 'scss', 'brands')
+    # config.assets.paths << Rails.root.join('node_modules', '@fortawesome', 'fontawesome-free', 'scss', 'v4-shims')
   end
 end
