b9adea75cd616d39711b71ac02e3553f6582f6b8
willamy388720 <willamygt@gmail.com>
2020-10-20 20:03:50 -0300

    showing more details of the services

--- app/controllers/backoffice/raxae_services_controller.rb
+++ app/controllers/backoffice/raxae_services_controller.rb
@@ -1,5 +1,19 @@
 class Backoffice::RaxaeServicesController < BackofficeController
+  before_action :set_service, only: %i[show edit update destroy]
+
   def index
     @services = Service.all
+  end
+
+  def show; end
+
+  private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def params_service
+    params.require(:raxae_services).permit(:title, :kind, :description, :value)
   end
 end
